ac220505b74930c989fe69b77cfae36bb1b32d7c
Koye <adeyombokehd@gmail.com>
2023-08-12 23:39:27 +0100

    "fin"

--- components/index.ts
+++ components/index.ts
@@ -2,10 +2,18 @@ import Hero from './Hero'
 import CustomButton from './CustomButton'
 import NavBar from './Navbar'
 import Footer from './Footer'
+import CustomFilter from './CustomFilter'
+import SearchBar from './SearchBar'
+import CarCard from './CarCard'
+import ShowMore from './ShowMore'
 
 export {
     Hero,
     CustomButton,
     NavBar,
-    Footer
+    Footer,
+    CustomFilter,
+    SearchBar,
+    CarCard,
+    ShowMore
 }
